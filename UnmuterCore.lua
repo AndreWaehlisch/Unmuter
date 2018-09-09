5668519eda314d500326c699b90263c3755332fe
André Wählisch <AndreWaehlisch@users.noreply.github.com>
2018-09-09 18:33:11 +0200

    rework internal implementation to minimize unwanted noise

--- UnmuterCore.lua
+++ UnmuterCore.lua
@@ -1,60 +1,40 @@
---Global port
+-- Global port
 Unmuter = {}
 
---set this to false to disable the addon temporarily - set it to true to re-enable it:
+-- set this to false to disable the addon temporarily - set it to true to re-enable it:
 Unmuter.enabled = true
 
 --define events
 Unmuter.Events = {
-	["UPDATE_BATTLEFIELD_STATUS"] = 1, --battleground and arena
-	["LFG_PROPOSAL_SHOW"] = 1, --lfg tool
-	["PARTY_INVITE_REQUEST"] = 1, --party invite
-	["DUEL_REQUESTED"] = 1, --duel invite
-	["READY_CHECK"] = 1, --ready check requested
-	["CONFIRM_SUMMON"]= 1, --player is summoned
-	["PET_BATTLE_QUEUE_PROPOSE_MATCH"] = 1, --pet battle invite
-	["LFG_LIST_APPLICANT_UPDATED"] = 1, -- new lfg tool
+	["UPDATE_BATTLEFIELD_STATUS"] = 1, -- battleground and arena
+	["LFG_PROPOSAL_SHOW"] = 1, -- lfg tool
+	["PARTY_INVITE_REQUEST"] = 1, -- party invite
+	["DUEL_REQUESTED"] = 1, -- duel invite
+	["READY_CHECK"] = 1, -- ready check requested
+	["CONFIRM_SUMMON"]= 1, -- player is summoned
+	["PET_BATTLE_QUEUE_PROPOSE_MATCH"] = 1, -- pet battle invite
 	["GROUP_INVITE_CONFIRMATION"] = 1, -- someone requests to join party (e.g. from friend list)
 }
 
-Unmuter.standardUnmuteTime = 2 --in seconds
+Unmuter.standardUnmuteTime = 2 -- in seconds
 
 Unmuter.TimerFrame = CreateFrame("Frame")
 
-Unmuter.Unmute = function(Soundtime, PlayNewSound, enableSFX)
-	local SFXSound_Old
-	local LastUpdate = 0
-
-	--unmute if sound is off
+Unmuter.Unmute = function()
+	-- unmute if sound is off
 	local AllSound_Old = GetCVar("Sound_EnableAllSound")
-	if (AllSound_Old == "0") then
+
+	if ( AllSound_Old == "0" ) then
+		local LastUpdate = 0
 		SetCVar("Sound_EnableAllSound", 1)
-	end
-
-	-- only enable SFX if requested
-	if ( enableSFX == true ) then
-		SFXSound_Old = GetCVar("Sound_EnableSFX")
-		if (SFXSound_Old == "0") then
-			SetCVar("Sound_EnableSFX", 1)
-		end
-	end
-
-	--play "new" sound when sound was off
-	if (SFXSound_Old == "0") or (AllSound_Old == "0") then
-		if ( PlayNewSound == true ) then
-			PlaySoundFile("Sound\\Interface\\iPlayerInviteA.ogg", "Master")
-		end
-
 		Unmuter.TimerFrame:SetScript("OnUpdate", function(self, elapsed)
 			LastUpdate = LastUpdate + elapsed
-			if LastUpdate > Soundtime then
-				--remute sound
+			if LastUpdate > Unmuter.standardUnmuteTime then
+				-- remute sound
 				if (AllSound_Old == "0") then
 					SetCVar("Sound_EnableAllSound", 0)
 				end
-				if (SFXSound_Old == "0") then
-					SetCVar("Sound_EnableSFX", 0)
-				end
+
 				self:SetScript("OnUpdate", nil)
 			end
 		end)
@@ -82,24 +62,22 @@ Unmuter.EventFrame:SetScript("OnEvent", function(self,event,...)
 			if ( GetBattlefieldStatus(i) == "confirm" ) then
 				if Unmuter.WasNotConfirm[i] then
 					Unmuter.WasNotConfirm[i] = false
-					Unmuter.Unmute(Unmuter.standardUnmuteTime, true, false)
+					Unmuter.Unmute()
+					PlaySoundFile("Sound\\Interface\\iPlayerInviteA.ogg", "Master")
 				end
 			else
 				Unmuter.WasNotConfirm[i] = true
 			end
 		end
-	elseif ( event == "LFG_LIST_APPLICANT_UPDATED" ) then
-			if ( UnitIsGroupLeader("player") or UnitIsGroupAssistant("player") ) then
-				local apps = C_LFGList.GetApplicants()
-				for i = 1, #apps do
-					local _, _, _, _, isNew = C_LFGList.GetApplicantInfo(apps[i])
-					if ( isNew ) then
-						Unmuter.Unmute(2*Unmuter.standardUnmuteTime, false, true)
-						return
-					end
-				end
-			end
 	else
-		Unmuter.Unmute(Unmuter.standardUnmuteTime, true, false)
+		Unmuter.Unmute()
+		PlaySoundFile("Sound\\Interface\\iPlayerInviteA.ogg", "Master")
+	end
+end)
+
+QueueStatusMinimapButton.EyeHighlightAnim:HookScript("OnLoop", function(self, loopState)
+	if ( Unmuter.enabled and QueueStatusMinimapButton_OnGlowPulse(self:GetParent()) ) then
+		Unmuter.Unmute(Unmuter.standardUnmuteTime, false, false)
+		PlaySound(SOUNDKIT.UI_GROUP_FINDER_RECEIVE_APPLICATION, "Master")
 	end
 end)
